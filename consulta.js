5d29cb605e1fb0e7fabeb24af4d3362b5c2a8252
davileal7 <108898208+davileal7@users.noreply.github.com>
2024-09-05 22:42:41 -0300

    update

--- consulta.js
+++ consulta.js
@@ -1,52 +1,50 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDwl0JvXHZMnLsic2zS2bu0Ne8jlqlRMZY",
-  authDomain: "consulta-cbb.firebaseapp.com",
-  databaseURL: "https://consulta-cbb-default-rtdb.firebaseio.com",
-  projectId: "consulta-cbb",
-  storageBucket: "consulta-cbb.appspot.com",
-  messagingSenderId: "564790071043",
-  appId: "1:564790071043:web:fa361e6e001fb523056336"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Referência para o banco de dados
-const database = firebase.database();
-
-import {getDatabase, ref, get, set, child, update, remove}
-from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-
-const db = getDatabase();
-
-var Buscar = document.getElementById("buscar_cnpj")
-var Numero_cnpj = document.getElementById("numero_cnpj")
-
-function SelectData(){
-  const dbref = ref(db);
-
-  get(child(dbref,Numero_cnpj.value)).then((snapshot) => {
-      if(snapshot.exists()){
-        
-      }
-      else {
-          alert("Não")
-      }
-  }).catch((error) => {
-      alert("falha, error"+error);
-  })
-
-}
-
-Buscar.addEventListener('click', SelectData)
+// Get references to the buttons and containers
+const btnCNPJ = document.getElementById('btnCNPJ');
+const btnCliente = document.getElementById('btnCliente');
+const cnpjContent = document.getElementById('cnpjContent');
+const clienteContent = document.getElementById('clienteContent');
+const firstContainer = document.querySelector('.container-consulta');
 
 
+// Create "X" buttons and append them after the respective h2 elements
+const cnpjXButton = document.getElementById('close');
+
+cnpjXButton.addEventListener('click', () => {
+  cnpjContent.style.display = 'none';
+  firstContainer.style.display = 'block';
+  document.getElementById('consulta-cnpj_cpf').value = '';
+   // Fechar os elementos
+   document.getElementById('cod').style.display = 'none';
+   document.getElementById('ficha').style.display = 'none';
+   document.getElementById('v1').style.display = 'none';
+   document.getElementById('v2').style.display = 'none';
+});
+cnpjContent.querySelector('h2').appendChild(cnpjXButton);
+
+const clienteXButton = document.getElementById('close2');
+clienteXButton.addEventListener('click', () => {
+  clienteContent.style.display = 'none';
+  firstContainer.style.display = 'block';
+  // Fechar os elementos
+  document.getElementById('consulta-cod_cli').value = '';
+  document.getElementById('ficha_cli').style.display = 'none';
+  document.getElementById('v1_cli').style.display = 'none';
+  document.getElementById('v2_cli').style.display = 'none';
+});
+clienteContent.querySelector('h2').appendChild(clienteXButton);
+
+
+// Add event listeners to the original buttons
+btnCNPJ.addEventListener('click', () => {
+  firstContainer.style.display = 'none';
+  cnpjContent.style.display = 'block';
+  clienteContent.style.display = 'none';
+});
+btnCliente.addEventListener('click', () => {
+  firstContainer.style.display = 'none';
+  clienteContent.style.display = 'block';
+  cnpjContent.style.display = 'none';
+});
 
 
 
